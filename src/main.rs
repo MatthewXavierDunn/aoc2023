8829a3fb000db5d2c2c858d02c73b0f9e12f4c57
Matthew Dunn <matthew.dunn.bkk@gmail.com>
2023-12-01 16:57:43 +0000

    Finish part 2

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,63 @@
-use std::io::{BufReader, stdin, BufRead};
+use std::io::{stdin, BufRead, BufReader};
 
-mod trebuchet;
+const NUMERICS: [&str; 9] = [
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+];
 
-fn get_calibration(string: &str) -> Option<u32> {
-    let first_digit = string.chars().find(|c| c.is_numeric())?.to_digit(10).unwrap();
-    let last_digit = string.chars().rev().find(|c| c.is_numeric())?.to_digit(10).unwrap();
-    Some(first_digit * 10 + last_digit)
+fn parse_numeric(string: &str) -> (Option<u8>, usize) {
+    for (i, numeric) in NUMERICS.into_iter().enumerate() {
+        if string.starts_with(numeric) {
+            return (Some(i as u8 + 1), 1);
+        }
+    }
+    if let Some(c) = string.chars().next() {
+        if c.is_ascii_digit() {
+            // Safety: since we have found a numeric character, conversion to a digit will always
+            // be safe.
+            unsafe {
+                return (Some(c.to_digit(10).unwrap_unchecked() as u8), 1);
+            }
+        }
+    }
+    return (None, 1);
+}
+
+fn get_calibration(mut string: &str) -> Option<u8> {
+    let mut first = None;
+    let mut last = None;
+
+    while !string.is_empty() {
+        let (res, advance) = parse_numeric(string);
+        match res {
+            Some(n) => {
+                if first.is_none() {
+                    first = Some(n);
+                }
+                last = Some(n);
+            }
+            None => (),
+        }
+        string = string.get(advance..).unwrap_or("");
+    }
+
+    if let (Some(first), Some(last)) = (first, last) {
+        Some(first * 10 + last)
+    } else {
+        None
+    }
 }
 
 fn main() -> std::io::Result<()> {
     let mut sum = 0;
-    for line in BufReader::new(stdin()).lines() {
-        let line = line?;
+    let stdin = stdin().lock();
+    let mut lines = BufReader::new(stdin).lines();
+    while let Some(Ok(line)) = lines.next() {
         if line.is_empty() {
             break;
         }
         if let Some(calibration) = get_calibration(&line) {
-            sum += calibration;
+            println!("{calibration} = {line}");
+            sum += calibration as u64;
         }
     }
     println!("{sum}");
