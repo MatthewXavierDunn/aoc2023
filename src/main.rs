9403ff1fa6a533860af917e7cd87d939e11509b0
Matthew Dunn <matthew.dunn.bkk@gmail.com>
2023-12-01 16:06:21 +0000

    Finish first problem

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use std::io::{BufReader, stdin, BufRead, ErrorKind};
+
+mod trebuchet;
+
+fn get_calibration(string: &str) -> Option<u32> {
+    let first_digit = string.chars().find(|c| c.is_numeric())?.to_digit(10).unwrap();
+    let last_digit = string.chars().rev().find(|c| c.is_numeric())?.to_digit(10).unwrap();
+    Some(first_digit * 10 + last_digit)
+}
+
+fn main() -> std::io::Result<()> {
+    let mut sum = 0;
+    for line in BufReader::new(stdin()).lines() {
+        let line = line?;
+        if line.is_empty() {
+            break;
+        }
+        if let Some(calibration) = get_calibration(&line) {
+            sum += calibration;
+        }
+    }
+    println!("{sum}");
+    Ok(())
+}
