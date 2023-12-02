3cc1a381df6a6d02015b760afe82b7d25326192a
Matthew Dunn <matthew.dunn.bkk@gmail.com>
2023-12-02 11:47:29 +0000

    Refactor solution

--- src/cube_conundrum.rs
+++ src/cube_conundrum.rs
@@ -1,69 +1,77 @@
-pub fn part1(mut lines: impl Iterator<Item = std::io::Result<String>>) {
-    let mut game_id_sum = 0;
-    while let Some(Ok(line)) = lines.next() {
-        if line.is_empty() {
-            break;
+struct Set {
+    red: u32,
+    green: u32,
+    blue: u32,
+}
+
+struct Game {
+    game_id: u32,
+    sets: Vec<Set>,
+}
+
+fn parse_set(string: &str) -> Set {
+    let mut red = 0;
+    let mut green = 0;
+    let mut blue = 0;
+
+    for count in string.split(", ") {
+        let (n, colour) = count.split_once(' ').unwrap();
+        let n: u32 = n.parse().unwrap();
+        match colour {
+            "red" => red += n,
+            "green" => green += n,
+            "blue" => blue += n,
+            _ => (),
         }
-        let (game_string, line) = line.split_once(": ").unwrap();
-        let (_, game_id) = game_string.split_once(" ").unwrap();
-        let game_id: u32 = game_id.parse().unwrap();
-        let sets = line.split("; ");
+    }
+
+    Set { red, green, blue }
+}
+
+fn parse_game(string: &str) -> Game {
+    let (game_string, line) = string.split_once(": ").unwrap();
+    let (_, game_id) = game_string.split_once(" ").unwrap();
+    let game_id: u32 = game_id.parse().unwrap();
+    let sets = line.split("; ").map(parse_set).collect();
+
+    Game { game_id, sets }
+}
+
+fn parse_games(lines: impl Iterator<Item = std::io::Result<String>>) -> impl Iterator<Item = Game> {
+    lines.filter_map(|line| {
+        line.ok()
+            .filter(|line| !line.is_empty())
+            .map(|line| parse_game(&line))
+    })
+}
+
+pub fn part1(lines: impl Iterator<Item = std::io::Result<String>>) {
+    let mut game_id_sum = 0;
+    for game in parse_games(lines) {
         let mut is_valid = true;
-        for set in sets {
-            let counts = set.split(", ");
-            let mut red_total = 0;
-            let mut green_total = 0;
-            let mut blue_total = 0;
-            for count in counts {
-                let (n, colour) = count.split_once(' ').unwrap();
-                let n: u32 = n.parse().unwrap();
-                match colour {
-                    "red" => red_total += n,
-                    "green" => green_total += n,
-                    "blue" => blue_total += n,
-                    _ => (),
-                }
-            }
-            if red_total > 12 || green_total > 13 || blue_total > 14 {
+        for set in game.sets {
+            if set.red > 12 || set.green > 13 || set.blue > 14 {
                 is_valid = false;
+                break;
             }
         }
         if is_valid {
-            game_id_sum += game_id;
+            game_id_sum += game.game_id;
         }
     }
     println!("{game_id_sum}");
 }
 
-pub fn part2(mut lines: impl Iterator<Item = std::io::Result<String>>) {
+pub fn part2(lines: impl Iterator<Item = std::io::Result<String>>) {
     let mut power_sum = 0;
-    while let Some(Ok(line)) = lines.next() {
-        if line.is_empty() {
-            break;
-        }
-        let (_, line) = line.split_once(": ").unwrap();
-        let sets = line.split("; ");
+    for game in parse_games(lines) {
         let mut max_red = 0;
         let mut max_green = 0;
         let mut max_blue = 0;
-        for set in sets {
-            let counts = set.split(", ");
-            for count in counts {
-                let (n, colour) = count.split_once(' ').unwrap();
-                let n: u32 = n.parse().unwrap();
-                match colour {
-                    "red" => if n > max_red {
-                        max_red = n;
-                    },
-                    "green" => if n > max_green {
-                        max_green = n;
-                    },
-                    "blue" => if n > max_blue {
-                        max_blue = n;
-                    },
-                    _ => (),
-                }
-            }
+        for set in game.sets {
+            max_red = max_red.max(set.red);
+            max_green = max_green.max(set.green);
+            max_blue = max_blue.max(set.blue);
         }
         power_sum += max_red * max_green * max_blue;
     }
