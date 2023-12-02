b9beae47ced0d200a16bcb81e9a4197013962205
Matthew Dunn <matthew.dunn.bkk@gmail.com>
2023-12-02 01:51:55 +0000

    Improve arg handling

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,19 @@
 use std::{
     env::args,
-    io::{stdin, BufRead, BufReader, ErrorKind},
+    io::{stdin, BufRead, BufReader},
 };
 
 mod trebuchet;
 
 fn main() -> std::io::Result<()> {
-    let mut args = args();
-    let _ = args.next();
-    let n = args.next().ok_or(ErrorKind::NotFound)?;
+    let mut args = args().skip(1);
+    let n = args.next().expect("Expected AoC identifier, e.g. 1-2");
     let stdin = stdin().lock();
     let lines = BufReader::new(stdin).lines();
     match n.as_str() {
         "1-1" | "trebuchet-part1" => trebuchet::part1(lines),
         "1-2" | "trebuchet-part2" => trebuchet::part2(lines),
-        _ => return Err(ErrorKind::Unsupported.into()),
+        _ => eprintln!("Unexpected AoC identifier '{n}'"),
     };
     Ok(())
 }
