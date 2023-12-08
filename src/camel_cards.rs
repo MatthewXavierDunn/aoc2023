117f848655fe8679d564d395032cd11bfbb4559a
Matthew Dunn <matthew.dunn.bkk@gmail.com>
2023-12-08 14:36:09 +0000

    Finish day 7 part 2

--- src/camel_cards.rs
+++ src/camel_cards.rs
@@ -37,80 +37,63 @@ impl From<char> for Card {
     }
 }
 
-impl From<usize> for Card {
-    fn from(n: usize) -> Self {
-        match n {
-            0 => Card::Ace,
-            1 => Card::King,
-            2 => Card::Queen,
-            3 => Card::Jack,
-            4 => Card::Ten,
-            5 => Card::Nine,
-            6 => Card::Eight,
-            7 => Card::Seven,
-            8 => Card::Six,
-            9 => Card::Five,
-            10 => Card::Four,
-            11 => Card::Three,
-            12 => Card::Two,
-            _ => Card::Two,
+#[repr(usize)]
+#[derive(Debug, Copy, Clone, PartialEq, PartialOrd, Eq, Ord)]
+enum CardJoker {
+    Ace,
+    King,
+    Queen,
+    Ten,
+    Nine,
+    Eight,
+    Seven,
+    Six,
+    Five,
+    Four,
+    Three,
+    Two,
+    Joker,
+}
+
+impl CardJoker {
+    fn is_joker(&self) -> bool {
+        match self {
+            Self::Joker => true,
+            _ => false,
         }
     }
 }
 
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
-struct Cards<const N: usize> {
-    cards: [Card; N],
-}
-
-impl<const N: usize> Cards<N> {
-    fn new(mut cards: [Card; N]) -> Self {
-        cards.sort();
-        Self { cards }
+impl From<char> for CardJoker {
+    fn from(c: char) -> Self {
+        match c {
+            'A' => CardJoker::Ace,
+            'K' => CardJoker::King,
+            'Q' => CardJoker::Queen,
+            'T' => CardJoker::Ten,
+            '9' => CardJoker::Nine,
+            '8' => CardJoker::Eight,
+            '7' => CardJoker::Seven,
+            '6' => CardJoker::Six,
+            '5' => CardJoker::Five,
+            '4' => CardJoker::Four,
+            '3' => CardJoker::Three,
+            '2' => CardJoker::Two,
+            'J' => CardJoker::Joker,
+            _ => CardJoker::Two,
+        }
     }
 }
 
 #[derive(Debug, PartialEq, PartialOrd, Eq, Ord)]
 enum HandType {
-    FiveOfAKind(Card),
-    FourOfAKind(Card, Card),
-    FullHouse(Card, Card),
-    ThreeOfAKind(Card, Cards<2>),
-    TwoPair(Cards<2>, Card),
-    OnePair(Card, Cards<3>),
-    HighCard(Cards<5>),
-}
-
-impl HandType {
-    fn kind_cmp(&self, other: &HandType) -> std::cmp::Ordering {
-        match (self, other) {
-            (Self::FiveOfAKind(..), Self::FiveOfAKind(..)) => std::cmp::Ordering::Equal,
-            (Self::FiveOfAKind(..), _) => std::cmp::Ordering::Less,
-            (_, Self::FiveOfAKind(..)) => std::cmp::Ordering::Greater,
-
-            (Self::FourOfAKind(..), Self::FourOfAKind(..)) => std::cmp::Ordering::Equal,
-            (Self::FourOfAKind(..), _) => std::cmp::Ordering::Less,
-            (_, Self::FourOfAKind(..)) => std::cmp::Ordering::Greater,
-
-            (Self::FullHouse(..), Self::FullHouse(..)) => std::cmp::Ordering::Equal,
-            (Self::FullHouse(..), _) => std::cmp::Ordering::Less,
-            (_, Self::FullHouse(..)) => std::cmp::Ordering::Greater,
-
-            (Self::ThreeOfAKind(..), Self::ThreeOfAKind(..)) => std::cmp::Ordering::Equal,
-            (Self::ThreeOfAKind(..), _) => std::cmp::Ordering::Less,
-            (_, Self::ThreeOfAKind(..)) => std::cmp::Ordering::Greater,
-
-            (Self::TwoPair(..), Self::TwoPair(..)) => std::cmp::Ordering::Equal,
-            (Self::TwoPair(..), _) => std::cmp::Ordering::Less,
-            (_, Self::TwoPair(..)) => std::cmp::Ordering::Greater,
-
-            (Self::OnePair(..), Self::OnePair(..)) => std::cmp::Ordering::Equal,
-            (Self::OnePair(..), _) => std::cmp::Ordering::Less,
-            (_, Self::OnePair(..)) => std::cmp::Ordering::Greater,
-
-            (Self::HighCard(..), Self::HighCard(..)) => std::cmp::Ordering::Equal,
-        }
-    }
+    FiveOfAKind,
+    FourOfAKind,
+    FullHouse,
+    ThreeOfAKind,
+    TwoPair,
+    OnePair,
+    HighCard,
 }
 
 impl From<Vec<Card>> for HandType {
@@ -119,82 +102,176 @@ impl From<Vec<Card>> for HandType {
             counts[*card as usize] += 1;
             counts
         });
-        let mut single = None;
-        let mut quad = None;
-        let mut triple = None;
-        let mut double = None;
-        let mut singles = [Card::Two; 5];
-        let mut singles_len = 0;
-        let mut pairs = [Card::Two; 2];
-        let mut pairs_len = 0;
-        for (card, &count) in counts.iter().enumerate() {
+        let mut single = false;
+        let mut quad = false;
+        let mut triple = false;
+        let mut double = false;
+        let mut singles = 0;
+        let mut pairs = 0;
+        for count in counts {
             match count {
-                5 => return Self::FiveOfAKind(card.into()),
+                5 => return Self::FiveOfAKind,
                 4 => {
-                    if let Some(single) = single {
-                        return Self::FourOfAKind(card.into(), single);
+                    if single {
+                        return Self::FourOfAKind;
                     }
-                    quad = Some(card.into());
+                    quad = true;
                 }
                 3 => {
-                    if let Some(double) = double {
-                        return Self::FullHouse(card.into(), double);
+                    if double {
+                        return Self::FullHouse;
                     }
-                    if singles_len == 2 {
-                        return Self::ThreeOfAKind(
-                            card.into(),
-                            Cards::new([singles[0], singles[1]]),
-                        );
+                    if singles == 2 {
+                        return Self::ThreeOfAKind;
                     }
-                    triple = Some(card.into());
+                    triple = true;
                 }
                 2 => {
-                    if let Some(triple) = triple {
-                        return Self::FullHouse(triple, card.into());
+                    if triple {
+                        return Self::FullHouse;
                     }
-                    if let Some(double) = double {
-                        if let Some(single) = single {
-                            return Self::TwoPair(Cards::new([double, card.into()]), single);
+                    if double {
+                        if single {
+                            return Self::TwoPair;
                         }
                     }
-                    if singles_len == 3 {
-                        return Self::OnePair(
-                            card.into(),
-                            Cards::new([singles[0], singles[1], singles[2]]),
-                        );
+                    if singles == 3 {
+                        return Self::OnePair;
                     }
-                    double = Some(card.into());
-                    pairs[pairs_len] = card.into();
-                    pairs_len += 1;
+                    double = true;
+                    pairs += 1;
                 }
                 1 => {
-                    if let Some(quad) = quad {
-                        return Self::FourOfAKind(quad, card.into());
+                    if quad {
+                        return Self::FourOfAKind;
                     }
-                    if let Some(triple) = triple {
-                        if let Some(single) = single {
-                            return Self::ThreeOfAKind(triple, Cards::new([single, card.into()]));
+                    if triple {
+                        if single {
+                            return Self::ThreeOfAKind;
                         }
                     }
-                    if pairs_len == 2 {
-                        return Self::TwoPair(Cards::new(pairs), card.into());
+                    if pairs == 2 {
+                        return Self::TwoPair;
                     }
-                    if let Some(double) = double {
-                        if singles_len == 2 {
-                            return Self::OnePair(
-                                double,
-                                Cards::new([singles[0], singles[1], card.into()]),
-                            );
+                    if double {
+                        if singles == 2 {
+                            return Self::OnePair;
                         }
                     }
-                    single = Some(card.into());
-                    singles[singles_len] = card.into();
-                    singles_len += 1;
+                    single = true;
+                    singles += 1;
                 }
                 _ => (),
             }
         }
-        Self::HighCard(Cards::new(singles))
+        Self::HighCard
+    }
+}
+
+impl From<Vec<CardJoker>> for HandType {
+    fn from(hand: Vec<CardJoker>) -> Self {
+        let mut jokers = 0;
+        let mut counts = hand.iter().fold([0u8; 12], |mut counts, card| {
+            if card.is_joker() {
+                jokers += 1;
+            } else {
+                counts[*card as usize] += 1;
+            }
+            counts
+        });
+        if jokers == 5 {
+            return Self::FiveOfAKind;
+        }
+        counts.sort_unstable();
+        counts.reverse();
+        let mut triple = false;
+        let mut singles = 0;
+        let mut pairs = 0;
+        for count in counts {
+            match count {
+                5 => return Self::FiveOfAKind,
+                4 => {
+                    if jokers == 1 {
+                        return Self::FiveOfAKind;
+                    }
+                    return Self::FourOfAKind;
+                }
+                3 => {
+                    if jokers == 2 {
+                        return Self::FiveOfAKind;
+                    }
+                    if jokers == 1 {
+                        return Self::FourOfAKind;
+                    }
+                    triple = true;
+                }
+                2 => {
+                    if jokers == 3 {
+                        return Self::FiveOfAKind;
+                    }
+                    if jokers == 2 {
+                        return Self::FourOfAKind;
+                    }
+                    if jokers == 1 {
+                        triple = true;
+                        jokers = 0;
+                    } else {
+                        if triple {
+                            return Self::FullHouse;
+                        }
+                        pairs += 1;
+                    }
+                }
+                1 => {
+                    if jokers == 4 {
+                        return Self::FiveOfAKind;
+                    }
+                    if jokers == 3 {
+                        return Self::FourOfAKind;
+                    }
+                    if jokers == 2 {
+                        triple = true;
+                        jokers = 0;
+                    } else {
+                        if jokers == 1 {
+                            pairs += 1;
+                            jokers = 0;
+                        } else {
+                            if triple && singles == 1 {
+                                return Self::ThreeOfAKind;
+                            }
+                            if pairs == 2 {
+                                return Self::TwoPair;
+                            }
+                            if pairs == 1 && singles == 2 {
+                                return Self::OnePair;
+                            }
+                            singles += 1;
+                        }
+                    }
+                }
+                _ => (),
+            }
+        }
+        Self::HighCard
+    }
+}
+
+#[derive(Debug, PartialEq, PartialOrd, Eq, Ord)]
+struct Hand<T> {
+    typ: HandType,
+    cards: [T; 5],
+}
+
+impl<T: Copy> From<Vec<T>> for Hand<T>
+where
+    Vec<T>: Into<HandType>,
+{
+    fn from(cards: Vec<T>) -> Self {
+        Self {
+            typ: cards.clone().into(),
+            cards: [cards[0], cards[1], cards[2], cards[3], cards[4]],
+        }
     }
 }
 
@@ -202,41 +279,7 @@ pub fn part1(lines: impl Iterator<Item = String>) {
     let mut hands = lines
         .map(|line| {
             let mut parts = line.split_whitespace();
-            let hand = parts
-                .next()
-                .unwrap()
-                .chars()
-                .map(|c| c.into())
-                .collect::<Vec<Card>>();
-            let hand_type: HandType = hand.clone().into();
-            let bid: u64 = parts.next().unwrap().parse().unwrap();
-            (hand_type, hand, bid)
-        })
-        .collect::<Vec<_>>();
-
-    hands.sort_by(|(hand_type1, hand1, _), (hand_type2, hand2, _)| {
-        match hand_type1.kind_cmp(hand_type2) {
-            std::cmp::Ordering::Equal => hand1.cmp(hand2),
-            ord => ord,
-        }
-    });
-
-    println!("{hands:?}");
-
-    let score = hands
-        .into_iter()
-        .rev()
-        .enumerate()
-        .map(|(rank, (_, _, bid))| (rank + 1) as u64 * bid)
-        .sum::<u64>();
-    println!("{score}");
-}
-
-pub fn part2(lines: impl Iterator<Item = String>) {
-    let mut hands = lines
-        .map(|line| {
-            let mut parts = line.split_whitespace();
-            let hand: HandType = parts
+            let hand: Hand<Card> = parts
                 .next()
                 .unwrap()
                 .chars()
@@ -250,7 +293,32 @@ pub fn part2(lines: impl Iterator<Item = String>) {
 
     hands.sort_by(|hand1, hand2| hand1.0.cmp(&hand2.0));
 
-    println!("{hands:?}");
+    let score = hands
+        .into_iter()
+        .rev()
+        .enumerate()
+        .map(|(rank, (_, bid))| (rank + 1) as u64 * bid)
+        .sum::<u64>();
+    println!("{score}");
+}
+
+pub fn part2(lines: impl Iterator<Item = String>) {
+    let mut hands = lines
+        .map(|line| {
+            let mut parts = line.split_whitespace();
+            let hand: Hand<CardJoker> = parts
+                .next()
+                .unwrap()
+                .chars()
+                .map(|c| c.into())
+                .collect::<Vec<_>>()
+                .into();
+            let bid: u64 = parts.next().unwrap().parse().unwrap();
+            (hand, bid)
+        })
+        .collect::<Vec<_>>();
+
+    hands.sort_by(|hand1, hand2| hand1.0.cmp(&hand2.0));
 
     let score = hands
         .into_iter()
